2b76e774ec05049c9644e874b91ec0c4aea5cc2e
Myles Borins <mylesborins@github.com>
2020-10-01 13:58:39 -0400

    move package.json checks to module

--- test/index.js
+++ test/index.js
@@ -15,3 +15,24 @@ tap.test('it resolves to false when there are gyp files', async t => {
   t.equal(await isNodeGypPackage(tempDir.name), false)
   tempDir.removeCallback()
 })
+
+tap.test('it resolves to false when there is a gypfile false in the package.json', async t => {
+  const tempDir = tmp.dirSync({unsafeCleanup: true})
+  fs.writeFileSync(tempDir.name + '/bindings.gyp', '')
+  t.equal(await isNodeGypPackage(tempDir.name, {gypfile: false}), false)
+  tempDir.removeCallback()
+})
+
+tap.test('it resolves to false when there is a custom install script', async t => {
+  const tempDir = tmp.dirSync({unsafeCleanup: true})
+  fs.writeFileSync(tempDir.name + '/bindings.gyp', '')
+  t.equal(await isNodeGypPackage(tempDir.name, {scripts: {install: 'echo 0'}}), false)
+  tempDir.removeCallback()
+})
+
+tap.test('it resolves to false when there is a custom preinstall script', async t => {
+  const tempDir = tmp.dirSync({unsafeCleanup: true})
+  fs.writeFileSync(tempDir.name + '/bindings.gyp', '')
+  t.equal(await isNodeGypPackage(tempDir.name, {scripts: {preinstall: 'echo 0'}}), false)
+  tempDir.removeCallback()
+})
