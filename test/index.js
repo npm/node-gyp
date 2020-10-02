cc635358d2564cb324b08bc34bc3b1b6b75d062f
isaacs <i@izs.me>
2020-10-02 12:27:28 -0700

    only respect binding.gyp files, not any *.gyp
    Fix: #1
    
    PR-URL: https://github.com/npm/node-gyp/pull/4
    Credit: @isaacs
    Close: #4
    Reviewed-by: @nlf

--- test/index.js
+++ test/index.js
@@ -3,10 +3,17 @@ const tmp = require('tmp')
 const fs = require('fs')
 const { isNodeGypPackage } = require('../lib')
 
-tap.test('it resolves to true when there are gyp files', async t => {
+tap.test('it resolves to true when there is a binding.gyp file', async t => {
   const tempDir = tmp.dirSync({unsafeCleanup: true})
-  fs.writeFileSync(tempDir.name + '/bindings.gyp', '')
+  fs.writeFileSync(tempDir.name + '/binding.gyp', '')
   t.equal(await isNodeGypPackage(tempDir.name), true)
+  tempDir.removeCallback()
+})
+
+tap.test('it resolves to false when there is a different *.gyp file', async t => {
+  const tempDir = tmp.dirSync({unsafeCleanup: true})
+  fs.writeFileSync(tempDir.name + '/blorpeedorp.gyp', '')
+  t.equal(await isNodeGypPackage(tempDir.name), false)
   tempDir.removeCallback()
 })
 
