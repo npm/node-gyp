ef6d6b7d878c5b48f8664a5c1fc6732b8e10d6f5
Jordan Harband <ljharb@gmail.com>
2021-02-08 13:27:45 -0800

    [fix] node v10.0 lacks `fs.promises`
    In this node version, fall back to `util.promisify` of the callback version.
    
    Maybe fixes https://github.com/npm/cli/issues/2623. Maybe fixes https://github.com/npm/cli/issues/2652. Maybe fixes https://github.com/npm/cli/issues/2625.

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ const tap = require('tap')
 const tmp = require('tmp')
 const fs = require('fs')
 const { isNodeGypPackage } = require('../lib')
+const requireInject = require('require-inject')
 
 tap.test('it resolves to true when there is a binding.gyp file', async t => {
   const tempDir = tmp.dirSync({unsafeCleanup: true})
@@ -21,4 +22,8 @@ tap.test('it resolves to false when there are gyp files', async t => {
   const tempDir = tmp.dirSync()
   t.equal(await isNodeGypPackage(tempDir.name), false)
   tempDir.removeCallback()
+})
+
+tap.test('it works when fs.promises is unavailable', async t => {
+  t.doesNotThrow(() => requireInject('../lib', { fs: { ...require('fs'), promises: null }}))
 })
