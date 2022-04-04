f7bd202c8abe1985d44e43268f336583f900be2e
Gar <gar+gh@danger.computer>
2022-04-04 07:55:04 -0700

    deps: @npmcli/template-oss@3.2.2
    BREAKING CHANGE: this drops support for node 10 and non-LTS versions of
    node 12 and node 14

--- test/index.js
+++ test/index.js
@@ -1,29 +1,24 @@
 const tap = require('tap')
-const tmp = require('tmp')
 const fs = require('fs')
-const { isNodeGypPackage } = require('../lib')
-const requireInject = require('require-inject')
+const { isNodeGypPackage } = require('..')
 
 tap.test('it resolves to true when there is a binding.gyp file', async t => {
-  const tempDir = tmp.dirSync({unsafeCleanup: true})
-  fs.writeFileSync(tempDir.name + '/binding.gyp', '')
-  t.equal(await isNodeGypPackage(tempDir.name), true)
-  tempDir.removeCallback()
+  const tempDir = t.testdir()
+  fs.writeFileSync(tempDir + '/binding.gyp', '')
+  t.equal(await isNodeGypPackage(tempDir), true)
 })
 
 tap.test('it resolves to false when there is a different *.gyp file', async t => {
-  const tempDir = tmp.dirSync({unsafeCleanup: true})
-  fs.writeFileSync(tempDir.name + '/blorpeedorp.gyp', '')
-  t.equal(await isNodeGypPackage(tempDir.name), false)
-  tempDir.removeCallback()
+  const tempDir = t.testdir()
+  fs.writeFileSync(tempDir + '/blorpeedorp.gyp', '')
+  t.equal(await isNodeGypPackage(tempDir), false)
 })
 
 tap.test('it resolves to false when there are gyp files', async t => {
-  const tempDir = tmp.dirSync()
-  t.equal(await isNodeGypPackage(tempDir.name), false)
-  tempDir.removeCallback()
+  const tempDir = t.testdir()
+  t.equal(await isNodeGypPackage(tempDir), false)
 })
 
 tap.test('it works when fs.promises is unavailable', async t => {
-  t.doesNotThrow(() => requireInject('../lib', { fs: { ...require('fs'), promises: null }}))
+  t.doesNotThrow(() => t.mock('../lib', { fs: { ...require('fs'), promises: null } }))
 })
